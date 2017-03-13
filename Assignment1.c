a0938d8166e023cd9cf529b59e5608f2fd52a567
Brandon Janson <b.janson42@hotmail.com>
2017-03-13 17:03:23 +1000

    help dialog and border for proper game complete

--- Assignment1.c
+++ Assignment1.c
@@ -46,26 +46,73 @@ char border_character = '#';
 // // (m) Declare a sprite_id called zombie.
 // sprite_id zombie;
 
-void draw_help_dialog(void) {
-	int height = 14;
-	int width = 78;
-
-
-}
-
 void draw_rectangle(int x, int y, int width, int height, char character) {
 	// int height = screen_height();
 	// int width = screen_width();
 
 	int left = x;
-	int right = width - 1;
+	int right = x + width - 1;
 	int top = y;
-	int bottom = height - 1;
+	int bottom = y + height - 1;
 
 	draw_line(left, top, right, top, character);
 	draw_line(right, top, right, bottom, character);
 	draw_line(right, bottom, left, bottom, character);
 	draw_line(left, bottom, left, top, character);
+}
+
+void draw_help_dialog(void) {
+	
+	// Dimensions as demonstrated in example video
+	int width = 64;
+	int height = 14;
+
+	// Get box around centre of the screen to accomodate
+	int x = (screen_width() - width) / 2;
+	int y = (screen_height() - height) / 2;
+
+	const int numStrings = 12;
+	const int maxLength = 40;
+
+	// Initalise string array to loop through easier
+	char strings[numStrings][maxLength] = {
+			"CAB202 Assignment 1",
+			"The Diamonds Of Doom",
+			"Brandon Janson",
+			"n9494006",
+			"",
+			"Controls",
+			"q\t: quit",
+			"h\t: help",
+			"   Arrow keys\t: move left/right",
+			"Space, z, x, c : shoot bullet    ",
+			"",
+			"Press a key to play..."
+		};
+	
+	// Draw the outline of the help dialog
+	draw_rectangle(x, y, width, height, border_character);
+
+	// Increment so that the strings are below the border
+	y++;
+
+	// Loop through the first four strings and display them on screen
+	for(int i = 0; i < numStrings; i++) {
+		int length = strlen(strings[i]);
+		draw_formatted((screen_width() - length) / 2, y + i, strings[i]);
+	}
+
+	// Increment by 4 so that y is now equal to the next line break
+	y += 4;
+
+	// Draw line break
+	draw_line(x, y, x + width - 1, y, border_character);
+
+	// Increment by 6 so that y is now equal to the next line break
+	y += 6;
+
+	// Draw line break
+	draw_line(x, y, x + width - 1, y, border_character);
 }
 
 void draw_border(void) {
@@ -74,6 +121,16 @@ void draw_border(void) {
 
 // Setup game.
 void setup(void) {
+	// Display the intial help dialog
+	draw_help_dialog();
+	show_screen();
+
+	// Wait for key input to start the game
+	int key = wait_char();
+
+	// Clear the screen ready for the game to start
+	clear_screen();
+
 	draw_border();
 }
 
