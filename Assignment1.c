20644ddbd2db2ae52d0c59d76e77ef6b554c46ad
Brandon Janson <b.janson42@hotmail.com>
2017-03-13 16:10:55 +1000

    frame works

--- Assignment1.c
+++ Assignment1.c
@@ -38,142 +38,48 @@ char * msg_image =
 /**/	"Goodbye and thank-you for playing ZombieDash Jr."
 /**/	"            Press any key to exit...            ";
 
-// (c) Declare a sprite_id called hero.
-sprite_id hero;
+char border_character = '#';
 
-// (m) Declare a sprite_id called zombie.
-sprite_id zombie;
+// // (c) Declare a sprite_id called hero.
+// sprite_id hero;
 
-void draw_border() {
-	int h = screen_height();
-	int w = screen_width();
+// // (m) Declare a sprite_id called zombie.
+// sprite_id zombie;
 
-	int left = 0;
-	int right = w - 1;
-	int top = 0;
-	int bottom = h - 1;
+void draw_help_dialog(void) {
+	int height = 14;
+	int width = 78;
 
-	draw_line(left, top, right, top, '*');
-	draw_line(right, top, right, bottom, '*');
-	draw_line(right, bottom, left, bottom, '*');
-	draw_line(left, bottom, left, top, '*');
+
+}
+
+void draw_rectangle(int x, int y, int width, int height, char character) {
+	// int height = screen_height();
+	// int width = screen_width();
+
+	int left = x;
+	int right = width - 1;
+	int top = y;
+	int bottom = height - 1;
+
+	draw_line(left, top, right, top, character);
+	draw_line(right, top, right, bottom, character);
+	draw_line(right, bottom, left, bottom, character);
+	draw_line(left, bottom, left, top, character);
+}
+
+void draw_border(void) {
+	draw_rectangle(0, 0, screen_width(), screen_height(), border_character);
 }
 
 // Setup game.
 void setup(void) {
-	// (a) Draw the border (setup).
 	draw_border();
-
-	// Keep the next line intact.
-	show_screen();
-
-	// (d) Set up the hero at the centre of the screen.
-	int hw = HERO_WIDTH, hh = HERO_HEIGHT;
-	hero = sprite_create((screen_width() - hw) / 2, (screen_height() - hh) / 2, hw, hh, hero_image);
-
-	// (e) Draw the hero.
-	sprite_draw(hero);
-
-	// Keep the next line intact.
-	show_screen();
-
-	// (n)	Set up the zombie at a random location on the screen.
-	int seed = time(NULL);
-	srand(seed);
-
-	int zx = rand() % (screen_width() - hw - 2);
-	int zy = rand() % (screen_height() - hh - 2);
-	zombie = sprite_create(zx, zy, ZOMBIE_WIDTH, ZOMBIE_HEIGHT, zombie_image);
-
-	// (o) Draw the zombie.
-	sprite_draw(zombie);
-
-	// (p) Set the zombie in motion.
-	sprite_turn_to(zombie, 0.2, 0);
-	int degrees = rand() % 360;
-	sprite_turn(zombie, degrees);
-
-	// Keep the next line intact.
-	show_screen();
 }
 
 // Play one turn of game.
 void process(void) {
-	// return;
-
-	// (f) Get a character code from standard input without waiting.
-	int key = get_char();
 	
-	// (y) Test for end of game.
-
-	// (g)	Get the current screen coordinates of the hero in integer variables 
-	//		by rounding the actual coordinates.
-	int hx = round(sprite_x(hero));
-	int hy = round(sprite_y(hero));
-
-	// (h) Move hero left according to specification.
-	if(key == 'a' && hx > 1) {
-		sprite_move(hero, -1, 0);
-	}
-
-	// (i) Move hero right according to specification.
-	if(key == 'd' && hx + HERO_WIDTH < screen_width() - 1) {
-		sprite_move(hero, 1, 0);
-	}
-
-	// (j) Move hero up according to specification.
-	if(key == 'w' && hy > 1) {
-		sprite_move(hero, 0, -1);
-	}
-
-	// (k) Move hero down according to specification.
-	if(key == 's' && hy + HERO_HEIGHT < screen_height() - 1) {
-		sprite_move(hero, 0, 1);
-	}
-
-	// (q.a) Test to move the zombie if key is 'z' or negative.
-	if(key == 'z' || key < 0) {
-		// (r) Zombie takes one step.
-		sprite_step(zombie);
-
-		// (s) Get screen location of zombie.
-		int zx = round(sprite_x(zombie));
-		int zy = round(sprite_y(zombie));
-
-		// (t) Get the displacement vector of the zombie.
-		double dx = sprite_dx(zombie);
-		double dy = sprite_dy(zombie);
-
-		// (u) Test to see if the zombie hit the left or right border.
-		if(zx == 0 || zx + ZOMBIE_WIDTH == screen_width() - 1) {
-			dx = -dx;
-		}
-
-		// (v) Test to see if the zombie hit the top or bottom border.
-		if(zy == 0 || zy + ZOMBIE_HEIGHT == screen_height()) {
-			dy = -dy;
-		}
-
-		// (w) Test to see if the zombie needs to step back and change direction.
-		if(dx != sprite_dx(zombie) || dy != sprite_dy(zombie)) {
-			sprite_back(zombie);
-			sprite_turn_to(zombie, dx, dy);
-		}
-
-	// (q.b) End else-if test to move the zombie if key is 'z' or negative.
-	}
-
-	// Leave next line intact
-	clear_screen();
-
-	// (b) Draw the border (process).
-	draw_border();
-
-	// (l)	Draw the hero.
-	sprite_draw(hero);
-
-	// (x)	Draw the zombie.
-	sprite_draw(zombie);
 }
 
 // Clean up game
